494a53ce95ce6cabd0f0081385a5b3734bbd66ec
Dimitar Rusev <mitikodev@gmail.com>
2023-03-23 00:54:55 +0200

    Solve challange 5 from set 1 by implementing repeating key XOR encryption

--- src/set1.rs
+++ src/set1.rs
@@ -96,3 +96,12 @@ fn challange4() {
     assert_eq!(key, 53);
     assert_eq!(output, "Now that the party is jumping\n");
 }
+
+#[test]
+fn challange5() {
+    let key = "ICE".as_bytes();
+    let input = "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+    let output = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+
+    assert_eq!(xor_rep(input.as_bytes(), key), hex_to_raw(output));
+}
--- src/utils.rs
+++ src/utils.rs
@@ -53,3 +53,7 @@ pub fn hex_to_base64(buf: &str) -> Vec<u8> {
 pub fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
     a.iter().zip(b.iter()).map(|(m, n)| m ^ n).collect()
 }
+
+pub fn xor_rep(data: &[u8], key: &[u8]) -> Vec<u8> {
+    data.iter().zip(key.iter().cycle()).map(|(x, y)| x ^ y).collect()
+}
