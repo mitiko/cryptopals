5e5da3669785d5cb1cfeca9e99145fdbca9e0b51
Dimitar Rusev <mitikodev@gmail.com>
2023-03-27 00:53:29 +0300

    Solve challange 10 from set 2 - do I need to detect and undo padding?

--- src/set2.rs
+++ src/set2.rs
@@ -1,4 +1,5 @@
-use crate::ecb::*;
+use crate::utils::io::*;
+use crate::{ecb::*, cbc::*};
 
 #[test]
 fn challange9() {
@@ -7,3 +8,12 @@ fn challange9() {
     assert_eq!(pkcs7_pad(input, 20), output);
 }
 
+#[test]
+fn challange10() {
+    let data = read_base64("data/set2/challange10.txt");
+    let key = b"YELLOW SUBMARINE";
+    let iv = b"\x00\x00\x00";
+    let decrypted = aes128_cbc_decrypt(key, iv, &data);
+    assert_eq!(&decrypted[decrypted.len() - 5..], b"\x0a\x04\x04\x04\x04");
+    std::fs::write("data/decoded/set2-challange10.txt", decrypted).unwrap();
+}
