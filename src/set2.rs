2a3e96afabaede0fc8e58ee03cbfaddc595c1858
Dimitar Rusev <mitikodev@gmail.com>
2023-04-03 01:20:11 +0300

    Challange 12 - detect block size & suffix length

--- src/set2.rs
+++ src/set2.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use rand::rngs::StdRng;
 use rand::{RngCore, Rng, SeedableRng};
 
-use crate::utils::io::*;
+use crate::utils::{io::*, conversions::*};
 use crate::{ecb::*, cbc::*};
 
 #[test]
@@ -75,4 +75,59 @@ fn challange11() {
         let (mode, ciphertext) = encryption_oracle(&plaintext, rng.gen());
         assert_eq!(mode, detection_oracle(&ciphertext));
     }
+}
+
+// vulnerable function
+fn ecb_random(plaintext: &[u8]) -> Vec<u8> {
+    let mut rng = rand::rngs::StdRng::from_seed([57; 32]);
+
+    let key = rng.gen();
+    let suffix_str = base64_to_raw("
+    Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkg
+    aGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBq
+    dXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUg
+    YnkK");
+
+    let data = {
+        let mut data = Vec::with_capacity(plaintext.len() + suffix_str.len());
+        data.extend_from_slice(plaintext);
+        data.extend_from_slice(&suffix_str);
+        pkcs7_pad(&data)
+    };
+
+    aes128_ecb_encrypt(&key, &data)
+}
+
+fn get_block_size() -> usize {
+    let initial_cipher_size = ecb_random(b"").len();
+    (1..(1 << 16))
+        .map(|i| ecb_random(&b"A".repeat(i)).len())
+        .find(|&cipher_size| cipher_size > initial_cipher_size)
+        .map(|cipher_size| cipher_size - initial_cipher_size)
+        .unwrap()
+}
+
+fn get_suffix_len() -> usize {
+    let initial_cipher_size = ecb_random(b"").len();
+    (1..16)
+        .map(|i| (i, ecb_random(&b"A".repeat(i)).len()))
+        .find(|&(_, cipher_size)| cipher_size > initial_cipher_size)
+        .map(|(prefix_len, _)| initial_cipher_size - prefix_len + 1)
+        .unwrap()
+}
+
+#[test]
+fn challange12() {
+    assert_eq!(get_block_size(), 16);
+    assert_eq!(detection_oracle(&b"0".repeat(16*4)), Mode::ECB);
+    let suffix_len = get_suffix_len();
+    assert_eq!(suffix_len, 138);
+
+    // let known_plaintext = Vec::new();
+
+    // use the current prefix + each byte from 0 to 255
+    // the one that matches the block_id with only the prefix is ours
+    // assert only one matches?
+
+    // append the new byte & move the block_id if needed (pos % 16 == 1)
 }
