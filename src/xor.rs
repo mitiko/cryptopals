897b59bcefd6c3f85c449921fa89ab6917fc014b
Dimitar Rusev <mitikodev@gmail.com>
2023-10-07 00:57:26 +0300

    Refactor XOR methods

--- src/xor.rs
+++ src/xor.rs
@@ -2,10 +2,10 @@ pub fn xor(a: &[u8], b: &[u8]) -> Vec<u8> {
     a.iter().zip(b.iter()).map(|(m, n)| m ^ n).collect()
 }
 
-pub fn xor_rep(data: &[u8], key: &[u8]) -> Vec<u8> {
-    data.iter().zip(key.iter().cycle()).map(|(x, y)| x ^ y).collect()
+pub fn xor_rep(a: &[u8], b: &[u8]) -> Vec<u8> {
+    a.iter().zip(b.iter().cycle()).map(|(x, y)| x ^ y).collect()
 }
 
 pub fn hamming_distance(a: &[u8], b: &[u8]) -> u32 {
-    xor(a, b).iter().map(|x| x.count_ones()).sum()
+    a.iter().zip(b.iter()).map(|(m, n)| m ^ n).map(|x| x.count_ones()).sum()
 }
