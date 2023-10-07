479b6112d36fedad70faf543234736ca685f9ce3
Dimitar Rusev <mitikodev@gmail.com>
2023-10-07 14:32:07 +0300

    Make AsU128 return a result with the inner error

--- src/utils/mod.rs
+++ src/utils/mod.rs
@@ -1,13 +1,15 @@
+use std::array::TryFromSliceError;
+
 pub mod conversions;
 pub mod io;
 
 pub trait AsU128 {
-    fn as_u128(self) -> Option<u128>;
+    fn as_u128(self) -> Result<u128, TryFromSliceError>;
 }
 
 impl AsU128 for &[u8] {
     /// Convert 16-byte blocks into u128, otherwise if size doesn't match None
-    fn as_u128(self) -> Option<u128> {
-        self.try_into().ok().map(|bytes| u128::from_be_bytes(bytes))
+    fn as_u128(self) -> Result<u128, TryFromSliceError> {
+        self.try_into().map(|bytes| u128::from_be_bytes(bytes))
     }
 }
